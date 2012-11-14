d5559c300594d577c6cae050f0fa4f411d0543f8
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2012-11-14 23:55:05 +0000

    Removed the 'users' module.

--- modules/users/manifests/groupmng.pp
+++
@@ -1,9 +0,0 @@
-define users::groupmng ($members) {
-  $group = $title
-  
-  # Ensure group has only members specified
-  exec { "gpasswd -M ${members} ${group}":
-    path    => ['/bin','/usr/bin','/sbin','/usr/sbin'],
-    require => [Group["$group"], User["${members}"]],
-  }
-}
