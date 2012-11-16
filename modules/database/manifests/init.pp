d38fa3b784f232b9ccbd55ea338704ecf3baa0f6
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2012-11-16 21:21:17 +0000

    Added the 'database' module to install & configure percona server.

---
+++ modules/database/manifests/init.pp
@@ -0,0 +1,52 @@
+# module: database
+
+class database {
+  $environment = $hostname ? {
+    /db\d?-dc0/ => 'prd',
+    default     => 'dev',
+  }
+
+  notify { "Environment: ${environment}": }
+
+  package { 'percona-release':
+    ensure => installed,
+    provider => rpm,
+    source => 'http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm'
+  }
+
+  package { 'Percona-Server-server-55':
+    ensure => '5.5.28-rel29.1.335.rhel6',
+    require => [ Package['percona-release'], Package['mysql-libs'] ]
+  }
+
+  package { 'Percona-Server-client-55':
+    ensure => '5.5.28-rel29.1.335.rhel6',
+    require => [ Package['percona-release'], Package['mysql-libs'] ]
+  }
+
+  package { 'rsync':      ensure => installed }
+
+  package { 'mysql-libs': ensure => absent    }
+
+  service { 'mysql':
+    ensure => running,
+    require => Package['Percona-Server-server-55']
+  }
+
+  file { '/etc/my.cnf':
+    content => template("database/${environment}.my.cnf.erb"),
+    owner   => 'mysql',
+    group   => 'mysql',
+    mode    => '644',
+    notify  => Service['mysql'],
+    require => Package['Percona-Server-server-55']
+  }
+
+  file { '/usr/local/bin/totsydbsync':
+    source  => 'puppet:///modules/database/totsydbsync',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '755'
+  }
+}
+
