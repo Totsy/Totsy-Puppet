7fe656fd761e65309ba3ecf2fb1c413f1ff9636e
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2012-11-20 18:15:01 +0000

    Moved logic from ERB template into puppet code, and fixed up nginx conf a bit (for devs anyway).

--- modules/app/manifests/vhost.pp
+++ modules/app/manifests/vhost.pp
@@ -1,6 +1,22 @@
 # module: app
 
 define app::vhost ($site = $title, $port = 80) {
+
+  $documentroot = regsubst($hostname, 'web-(.+)', '\1.totsy.com')
+  $servername   = regsubst($hostname, 'web-(.+)', '\1.totsy.com')
+
+  if $site != 'totsy' {
+    $servername = "#{$site}-#{$servername}"
+  } else {
+    cron { 'magento':
+      command => "/usr/bin/php /var/www/$documentroot/cron.php",
+      user    => 'nginx',
+      hour    => '*',
+      minute  => '*/5',
+      require => Package['nginx']
+    }
+  }
+
   file { "/etc/nginx/sites-available/$site":
     content => template('app/site-totsy.conf.erb'),
     owner   => 'nginx',
